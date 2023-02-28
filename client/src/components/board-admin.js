4237cdc8927059c5b9b88cb55dafeafaa53d211a
martinwachira <martinnwachira@gmail.com>
2023-02-28 17:48:41 +0300

    admin --dash

--- client/src/components/board-admin.js
+++ client/src/components/board-admin.js
@@ -1,11 +1,24 @@
 import React, { useEffect, useState } from "react";
 
+import AuthService from "../services/auth.service";
+import { Navigate } from "react-router-dom";
 import UserService from "../services/user.service";
 
 const BoardAdmin = () => {
   const [content, setContent] = useState("");
+  const [currentUser, setCurrentUser] = useState({ username: "" });
+  const [userReady, setUserReady] = useState(false);
+  const [redirect, setRedirect] = useState(null);
 
   useEffect(() => {
+    const currentUser = AuthService.getCurrentUser();
+
+    if (!currentUser) {
+      setRedirect("/home");
+    } else {
+      setCurrentUser(currentUser);
+      setUserReady(true);
+    }
     UserService.getAdminBoard().then(
       (response) => {
         setContent(response.data);
@@ -22,10 +35,43 @@ const BoardAdmin = () => {
     );
   }, []);
 
+  if (redirect) {
+    return <Navigate to={redirect} />;
+  }
+
+  console.log("current user", currentUser);
+
   return (
     <div className="container">
       <header className="jumbotron">
         <h3>{content}</h3>
+        <table class="table">
+          <thead class="thead-light">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Username</th>
+              <th scope="col">Email</th>
+              <th scope="col">Role</th>
+              <th scope="col">Access Token</th>
+              <th scope="col">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>{currentUser.username}</td>
+              <td>{currentUser.email}</td>
+              <td>{currentUser.role}</td>
+              <td>
+                {currentUser.accessToken.substring(0, 10)} ...{" "}
+                {currentUser.accessToken.substr(
+                  currentUser.accessToken.length - 10
+                )}
+              </td>
+              <td>Edit</td>
+            </tr>
+          </tbody>
+        </table>
       </header>
     </div>
   );
