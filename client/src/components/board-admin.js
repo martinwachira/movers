3a4edf41b7663eb19d12613b4267b29b6fb9344c
martinwachira <martinnwachira@gmail.com>
2023-03-04 15:19:02 +0300

    fetch all bookings --client

--- client/src/components/board-admin.js
+++ client/src/components/board-admin.js
@@ -183,6 +183,50 @@ const BoardAdmin = () => {
         </table>
         <br />
         <p style={{ color: "#f09a53", fontWeight: "bolder" }}>Bookings</p>
+        <table className="table">
+          <thead class="thead-light">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Done By</th>
+              <th scope="col">Date</th>
+              <th scope="col">PickupTime</th>
+              <th scope="col">Pickup Location</th>
+              <th scope="col">Destination</th>
+              <th scope="col">Vehicle</th>
+              <th scope="col">Created At</th>
+              <th scope="col">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {bookings &&
+              bookings?.map((booking, index) => (
+                <tr key={booking.id}>
+                  <th>{index + 1}</th>
+                  <td>
+                    {booking.user.username.charAt(0).toUpperCase() +
+                      booking.user.username.slice(1)}
+                  </td>
+                  <td> {booking.bookingDate}</td>
+                  <td>{booking.pickupTime}</td>
+                  <td>
+                    {booking.pickupLocation.charAt(0).toUpperCase() +
+                      booking.pickupLocation.slice(1)}
+                  </td>
+                  <td>
+                    {booking.destination.charAt(0).toUpperCase() +
+                      booking.destination.slice(1)}
+                  </td>
+                  <td> {booking.vehicle.vname.toUpperCase()}</td>
+                  <td>{new Date(booking.createdAt).toDateString()}</td>
+                  <td>
+                    <i class="bi bi-pencil-square"></i>
+                    &nbsp; &nbsp;
+                    <i class="bi bi-trash-fill"></i>
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
